b54a18176fa5a5a44518cd2629f63b8780b501aa
Oleksandr Voznyi <32833132+alexmanri1@users.noreply.github.com>
2023-03-18 16:08:36 +0200

    Delete Lab_2 directory

--- Lab_2/Лаб-2. Завдання 4/tests/q6.py
+++
@@ -1,3 +0,0 @@
-OK_FORMAT = True
-
-test = {'name': 'q6', 'points': 1, 'suites': [{'cases': [{'code': '>>> kol_uniq_symb == 26\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
